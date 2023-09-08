48948ffea7ef28879c092df6467e977c82ad7410
Janos Hidvegi <jani.hidvegi@gmail.com>
2023-09-08 15:22:28 +0200

    wip

--- src/api/generateRecipes.ts
+++ src/api/generateRecipes.ts
@@ -2,7 +2,7 @@ export async function generateRecipes(ingredients: string[]) {
     "use client"
 
     // Call the chatgpt api to generate recipes based on ingredients
-    const apiKey = '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'; // Make sure to store your API key in a .env.local file
+    const apiKey = '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'; // Make sure to store your API key in a .env.local file
     const prompt = `Generate a recipe using the following ingredients: ${ingredients.join(', ')}. Reply with HTML only. No other text.`;
 
     const response = await fetch('https://api.openai.com/v1/completions', {
