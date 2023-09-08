43e8c568f0e1c30e2afc8299a3825fe67c304a86
Janos Hidvegi <jani.hidvegi@gmail.com>
2023-09-08 15:25:43 +0200

    wip

--- src/api/generateRecipes.ts
+++ src/api/generateRecipes.ts
@@ -1,23 +1,24 @@
 export async function generateRecipes(ingredients: string[]) {
     "use client"
-
-    // Call the chatgpt api to generate recipes based on ingredients
-    const apiKey = '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'; // Make sure to store your API key in a .env.local file
-    const prompt = `Generate a recipe using the following ingredients: ${ingredients.join(', ')}. Reply with HTML only. No other text.`;
-
-    const response = await fetch('https://api.openai.com/v1/completions', {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${apiKey}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt,
-            model: "text-davinci-003",
-            max_tokens: 500, // Limit to 500 tokens, adjust as needed
-        }),
-    });
-
-    const data = await response.json();
-    return data.choices?.[0]?.text?.trim();
+return "<h1>Delicious Pepper Butter Recipe</h1>\n" +
+    "        <p>This mouthwatering pepper butter recipe combines the rich flavors of butter with the vibrant colors and flavors of red and yellow peppers.</p>\n" +
+    "        \n" +
+    "        <h2>Ingredients:</h2>\n" +
+    "        <ul>\n" +
+    "            <li>2 tablespoons of butter</li>\n" +
+    "            <li>1 red pepper, diced</li>\n" +
+    "            <li>1 yellow pepper, diced</li>\n" +
+    "        </ul>\n" +
+    "\n" +
+    "        <h2>Instructions:</h2>\n" +
+    "        <ol>\n" +
+    "            <li>In a medium-sized pan, melt the butter over medium heat.</li>\n" +
+    "            <li>Add the diced red and yellow peppers to the melted butter.</li>\n" +
+    "            <li>Sauté the peppers for about 5-7 minutes until they become tender and slightly caramelized.</li>\n" +
+    "            <li>Remove from heat and let the pepper butter cool for a few minutes.</li>\n" +
+    "            <li>Transfer the pepper butter to a small jar or container for serving.</li>\n" +
+    "        </ol>\n" +
+    "\n" +
+    "        <p>Your delicious pepper butter is ready to serve! It's perfect as a spread for bread, a topping for grilled meats, or as a flavorful addition to your favorite dishes.</p>\n" +
+    "    ";
 }
